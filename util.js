474d30ae3e49b5aeaac318485c471920e17629f1
Anna Lee <annalee@andrew.cmu.edu>
2024-01-16 11:05:05 -0500

    Added email button

--- util.js
+++ util.js
@@ -1,14 +1,19 @@
 function generateEmail() {
-    return 'lmao';
+    const address_ascii = [97, 106, 117, 110, 0, 108, 98, 101];
+    address_ascii[4] = address_ascii[0] + 4;
+    address_ascii[6] += 3;
+    var address = address_ascii.map((x) => String.fromCharCode(x)).join(""); 
+
+    const provider_ascii = [108, 117, 116, 54, 111, 111, 107];
+    provider_ascii[0] += 3;
+    provider_ascii[3] *= 2;
+    var provider = provider_ascii.map((x) => String.fromCharCode(x)).join("");
+
+    return address + "@" + provider + ".com";
+
 }
 
-function copyToClipboard() {
-    navigator.clipboard.writeText(generateEmail());
-    var tooltip = document.getElementById("emailTooltip");
-    tooltip.innerHTML = "Copied email to clipboard!";
-}
-
-function displayTooltip() {
-    var tooltip = document.getElementById("emailTooltip");
-    tooltip.innerHTML = "Copy email to clipboard";
+function openMailTo() {
+    window.location = "mailto:" + generateEmail();
+    return null;
 }
